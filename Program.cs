6ccdb9a51f7b0147ddfc687c63015f430f7d1012
User <User@DESKTOP-ESP2JT1>
2022-11-08 16:17:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace while_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "Grigor Abadjiev";
+            string password = "123";
+
+            Console.WriteLine("Enter password");
+            string input = Console.ReadLine();
+
+            while (input != password)
+            {
+                Console.WriteLine("Wrong password");
+                Console.WriteLine("Enter password");
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Hello {username}!");
+        }   
+    }
+}
